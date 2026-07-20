36bb76192b259921c2f3c9b1adb5ca42937d369a
BeginnersPythonCourse <a.cowan853@gmail.com>
2026-07-20 22:27:18 +0100

    Create database tables on app startup

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
-from app.api import routes_health
-from app.api import routes_sales
+
+from app.api import routes_health, routes_sales
+from app.db import models
+from app.db.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Whisky Market Data API",
     description="API for uploading, validating and analysing whisky market data.",
-    version="0.1.0"
+    version="0.1.0",
 )
 
 app.include_router(routes_health.router)
